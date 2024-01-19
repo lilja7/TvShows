b4113c71c849f54d2cb32eb3e0dc98e15e825e95
Lilja <lilja9868@gmail.com>
2024-01-19 16:42:55 +0000

    ??

--- TvShows/Program.cs
+++ TvShows/Program.cs
@@ -1,11 +1,17 @@
+using TvShows.Data;
+using TvShows.Data.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
 builder.Services.AddControllers();
+builder.Services.AddScoped<TvShowsRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
